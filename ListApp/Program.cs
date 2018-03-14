3ebf229c6d72570ac3250a77bd95c2814a01ae72
igor-zhylin <37139257+igor-zhylin@users.noreply.github.com>
2018-03-14 22:54:33 +0200

    BaseStructureUpdated
    Added DLL and create class list

---
+++ ListApp/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ListApp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using ObjectList;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List list = new List();
+        }
+    }
+}
---
+++ ObjectList/List.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectList
+{
+    public class List
+    {
+        private object[] _array;
+        private const int DefaultArraySize = 4;
+        public int Length { get { return Length; } }
+
+        public List()
+        {
+            _array = new object[DefaultArraySize];
+        }
+
+        public object this[int index]
+        {
+            get { return _array[index]; }
+        }
+
+        public void Add(object objectToAdd)
+        {
+            if (objectToAdd == null)
+            {
+                throw new ArgumentNullException("Object cannot be NULL!");
+            }
+            
+        }
+
+        private void ResizeErray()
+        {
+        }
+    }
+}
