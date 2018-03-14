5be7fdf204470ee2dfd1b14173556976b45c9ff9
igor-zhylin <37139257+igor-zhylin@users.noreply.github.com>
2018-03-15 00:11:43 +0200

    First Update
    Added :
    Add
    Insert
    Length

--- ListApp/Program.cs
+++ ListApp/Program.cs
@@ -1,10 +1,6 @@
 ﻿namespace ListApp
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
     using ObjectList;
 
     class Program
@@ -12,6 +8,17 @@
         static void Main(string[] args)
         {
             List list = new List();
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(i + 1);
+            }
+            list.Insert(2, 4);
+
+            for (int i = 0; i < list.Length; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+            Console.ReadKey();
         }
     }
 }
--- ObjectList/List.cs
+++ ObjectList/List.cs
@@ -1,20 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿/// <summary>
+/// Created By Zhylin
+/// </summary>
 namespace ObjectList
 {
+    using System;
+
     public class List
     {
         private object[] _array;
-        private const int DefaultArraySize = 4;
-        public int Length { get { return Length; } }
+        private const int DefaultArrayListSize = 4;
+        private const int ArrayLengthMultiplier = 2;
+        public int Length { get; private set; }
+
 
         public List()
         {
-            _array = new object[DefaultArraySize];
+            _array = new object[DefaultArrayListSize];
         }
 
         public object this[int index]
@@ -24,15 +25,68 @@ namespace ObjectList
 
         public void Add(object objectToAdd)
         {
-            if (objectToAdd == null)
+            CheckObjectZeroValues(objectToAdd);
+            if (Length < _array.Length)
+            {
+                AddElement(objectToAdd);
+            }
+            else
+            {
+                ResizeErray();
+                AddElement(objectToAdd);
+            }
+        }
+
+        private void CheckObjectZeroValues(object obj)
+        {
+            if (obj == null)
             {
                 throw new ArgumentNullException("Object cannot be NULL!");
             }
-            
+        }
+
+        private void AddElement(object obj)
+        {
+            for (int i = 0; i < _array.Length; i++)
+            {
+                if (_array[i] == null)
+                {
+                    _array[i] = obj;
+                    Length++;
+                    return;
+                }
+            }
         }
 
         private void ResizeErray()
         {
+            var resizedArray = new object[Length * ArrayLengthMultiplier];
+            for (int i = 0; i < _array.Length; i++)
+            {
+                resizedArray[i] = _array[i];
+            }
+            _array = resizedArray;
+        }
+
+        public void Insert(int index, object objectToInsert)
+        {
+            CheckObjectZeroValues(objectToInsert);
+            if (Length == _array.Length)
+            {
+                ResizeErray();
+                InsertElement(index, objectToInsert);
+            }
+            InsertElement(index, objectToInsert);
+        }
+
+        private void InsertElement(int index, object element)
+        {
+            for (int i = Length; i >= index; --i)
+            {
+                _array[i + 1] = _array[i];
+            }
+            _array[index] = element;
+            Length++;
         }
     }
 }
